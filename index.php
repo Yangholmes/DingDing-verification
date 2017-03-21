f709accb80ea5efe927ebc20f37cbed83b9cea5a
Yangholmes <yangholmes@126.com>
2017-03-21 16:39:21 +0800

    DingDing Auth verification app.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<?php
+require_once( __DIR__.'/server/api/Auth.php');
+require_once( __DIR__.'/server/api/User.php');
+?>
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>通导用车</title>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="img/icon/car.ico" />
+</head>
+
+<body>
+  <p class="notification">正在验证您的身份，请稍后……</p>
+
+  <!-- script start -->
+  <script>
+    var _config =
+      <?php
+        $auth = new Auth(1);  // debug: 1表示本地调试；0表示远程服务器。使用本地调试时，请注意修改config文件
+        echo json_encode($auth->get_signature());
+      ?>;
+    localStorage.setItem('thisUser', 'error');
+  </script>
+
+  <script src="http://g.alicdn.com/dingding/open-develop/1.0.0/dingtalk.js"></script>
+  <script src="./lib/jquery/jquery-3.1.1.js"></script>
+  <script src="js/dd-init.js"></script>
+  <!-- script end -->
+</body>
---
+++ js/dd-init.js
@@ -0,0 +1,124 @@
+;
+
+/**
+ * super global variables
+ * _config  √
+ * _user    √
+ * _car     °
+ */
+var _user = {}, // user info
+    _config = _config; // dd config
+
+/**
+ * jsapi权限验证配置
+ * _config 是保存dd配置的全局变量
+ */
+dd.config({
+    agentId: _config.agentId[0], // this app ID
+    corpId: _config.corpId[0],
+    timeStamp: _config.timeStamp,
+    nonceStr: _config.nonceStr,
+    signature: _config.signature, // jsapi signature
+    type: 0, //选填。0表示微应用的jsapi,1表示服务窗的jsapi。
+    jsApiList: [ // 需要调用的jsapi列表
+        'runtime.info',
+        'biz.contact.choose',
+        'device.notification.confirm',
+        'device.notification.alert',
+        'device.notification.prompt',
+        'biz.ding.post',
+        'biz.util.openLink',
+        'ui.pullToRefresh.enable',
+        'ui.pullToRefresh.stop',
+        'biz.util.openLink',
+        'biz.navigation.setLeft',
+        'biz.navigation.setTitle',
+        'biz.navigation.setRight'
+    ]
+}); // jsapi permission
+
+/**
+ * 钉钉入口
+ */
+dd.ready(function() {
+
+    /**
+     * 容器
+     */
+    // 获取容器信息
+    dd.runtime.info({
+        onSuccess: function(info) {
+            console.log('runtime info: ' + JSON.stringify(info));
+        },
+        onFail: function(err) {
+            console.log('fail: ' + JSON.stringify(err));
+        }
+    }); // runtime info
+
+    // 获取微应用免登授权码、登陆用户信息
+    dd.runtime.permission.requestAuthCode({
+        corpId: _config.corpId[0],
+        onSuccess: function(result) {
+            console.log('微应用免登授权码: ', result);
+            $.ajax({
+                url: "server/verification/get-user-info.php?access_token=" + _config.accessToken + "&code=" + result.code,
+                method: 'GET',
+                dataType: 'json',
+                success: function(respond) {
+                    $('p.notification').html('验证成功！going back~');
+                    _user = respond; //
+                    localStorage.setItem('thisUser', JSON.stringify(_user));
+                    window.history.back(); return false; // Safari要求 return false 方可回退
+                },
+                error: function() {
+                  $('p.notification').html('验证失败！');
+                  localStorage.setItem('thisUser', 'error');
+                  window.history.back(); return false;
+                }
+            });
+        },
+        onFail: function(err) {
+            console.log('微应用免登授权码, 错误: ', err);
+        }
+
+    });
+
+    /**
+     * 下拉刷新
+     */
+    dd.ui.pullToRefresh.disable();
+
+    /**
+     * 标题栏
+     */
+    dd.biz.navigation.setTitle({
+        title: '身份认证',
+        onSuccess: function(data) {
+            console.log(data);
+        },
+        onFail: function(err) {
+            console.log(JSON.stringify(err));
+        }
+    }); // set navigation title
+
+    /**
+     * 导航栏设置
+     */
+     dd.biz.navigation.setRight({
+     show: false,//控制按钮显示， true 显示， false 隐藏， 默认true
+     control: true,//是否控制点击事件，true 控制，false 不控制， 默认false
+     text: '',//控制显示文本，空字符串表示显示默认文本
+     onSuccess : function(result) {},
+     onFail : function(err) {}
+ });
+
+    /**
+     * 其他业务
+     */
+
+});
+
+dd.error(function(err) {
+    console.log('错误信息: ' + JSON.stringify(err));
+    alert('错误信息: ' + JSON.stringify(err));
+});
